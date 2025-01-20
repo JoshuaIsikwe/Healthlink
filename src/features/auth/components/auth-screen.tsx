e3ad5f1946cd640d90ac7bcc657a3815f0d26a24
JoshuaIsikwe <IsikweJoshua@gmail.com>
2025-01-20 18:10:03 +0200

    Login

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { SignInFlow } from '../types'
 import { SignInCard } from './sign-in-card'
 import { SignUpCard } from './sign-up-card'
+import pic from '../../../assets/images/healthLink.jpg'
 
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn")
   
 
   return (
-    <div className="h-full flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url(/assets/images/healthLink.jpg)' }}>
+    <div className="h-full flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${pic})`, opacity: 0.8 }}>
       <div className="md:h-auto md:w-[420px]">
        {state === "signIn" ? <SignInCard setState={setState}/> : <SignUpCard setState={setState}/>}
       </div>
